274e1ed9a19874067b255ae908e6b1ad1a1dbee4
safanador <sergioafanador102@gmail.com>
2024-09-21 19:18:42 -0500

    description updated

--- app/page.tsx
+++ app/page.tsx
@@ -86,13 +86,20 @@ export default function Home() {
           </div>
           <div className="w-full pr-4">
             <p className="mb-4 text-gray-300">
-              Back in 2012, I decided to try my hand at creating custom Tumblr themes and tumbled head first into the rabbit hole of coding and web development. Fast-forward to today, and I’ve had the privilege of building software for an advertising agency, a start-up, a huge corporation, and a digital product studio.
+              Soy Desarrollador Frontend junior con experiencia en desarrollo de interfaces accesibles y
+              responsivas adaptadas a diferentes dispositivos y tamaños de pantalla para la web y móviles,
+              usando React y React-Native, tengo experiencia realizando integraciones de API y manejo de
+              estados tanto en web como en móvil.
             </p>
             <p className="mb-4 text-gray-300">
-              My main focus these days is building accessible user interfaces for our customers at Klaviyo. I most enjoy building software in the sweet spot where design and engineering meet — things that look good but are also built well under the hood. In my free time, I have also released an online video course that covers everything you need to know to build a web app with the Spotify API.
+              Me caracterizo por mi capacidad para escribir código
+              eficiente, modular y reutilizable, asimismo, me caracterizo por estar abierto al uso y aprendizaje
+              de tecnologías diferentes a las que frecuentemente uso.
             </p>
             <p className="text-gray-300">
-              When I’m not at the computer, I’m usually rock climbing, reading, hanging out with my wife and two cats, or running around Hyrule searching for Korok seeds.
+              Me identifico como una persona capaz de solucionar problemas, atenta al detalle, capaz de
+              cumplir con tiempos de entrega y de trabajar en equipo o individualmente en entornos agiles y de
+              ritmo rápido.
             </p>
           </div>
         </section>
